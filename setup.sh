f0ec936b77abb0ed2ff54e61a0a3075c82fba7c2
yajamon <yajamon.tatsuki@gmail.com>
2016-11-24 19:50:44 +0900

    Edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-DOT_FILES=(.bashrc .bash_profile)
+
+DOT_FILES=($HOME/dotfiles/.??* bin)
 
 for file in ${DOT_FILES[@]}
 do
+    file=$(basename $file)
+    [[ $file == *.DS_Store ]] && continue
+    [[ $file == .git ]] && continue
+    [[ $file == .gitignore ]] && continue
+
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
