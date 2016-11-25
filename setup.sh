bfad839dbb970f46ef3f83f308e7afcbef2264d4
yajamon <yajamon.tatsuki@gmail.com>
2016-11-25 17:02:14 +0900

    Update setup.sh
    display information when symbolic link referring to an unexpected place:
    grouped echo command and put to stderr

--- setup.sh
+++ setup.sh
@@ -12,7 +12,10 @@ do
 
     if [ -L $HOME/$file ] ; then
         [ "$(readlink $HOME/$file)" = "$DOT_PATH/$file" ] && echo "exists link: $HOME/$file"
-        [ "$(readlink $HOME/$file)" != "$DOT_PATH/$file" ] && echo "exists link: $HOME/$file" && echo "but, look different location: $(readlink $HOME/$file)"
+        [ "$(readlink $HOME/$file)" != "$DOT_PATH/$file" ] && {
+            echo "exists link: $HOME/$file"
+            echo "but, look different location: $(readlink $HOME/$file)"
+        } 1>&2
         continue
     fi
     [ -e $HOME/$file ] && echo "exists file: $HOME/$file" && continue
