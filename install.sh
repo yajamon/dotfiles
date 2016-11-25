bcfd9fd85ba1878fabf85ce2c791dcfb9f6d4ad6
yajamon <yajamon.tatsuki@gmail.com>
2016-11-25 20:26:53 +0900

    small fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 DOTFILES_REPO="https://github.com/yajamon/dotfiles.git"
-git clone --recursive $DOTFILES_REPO $HOME/.dotfiles
+git clone --recursive $DOTFILES_REPO $HOME/dotfiles
 ./$HOME/.dotfiles/setup.sh
