9f68e8587af9e50e93b120682730929024f542b9
yajamon <yajamon.tatsuki@gmail.com>
2017-07-27 16:21:38 +0900

    Fix failed execute init script

--- etc/init/linux.sh
+++ etc/init/linux.sh
@@ -4,7 +4,7 @@ DOT_PATH=$HOME/dotfiles
 INIT_SCRIPTS=$DOT_PATH/etc/init
 
 if type -a yum >/dev/null 2>&1 ; then
-    $INIT_SCRIPTS/yum.sh
+    bash $INIT_SCRIPTS/yum.sh
 fi
 
-$INIT_SCRIPTS/dein.vim.sh
+bash $INIT_SCRIPTS/dein.vim.sh
