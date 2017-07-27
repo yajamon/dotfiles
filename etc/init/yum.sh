eff5377edf7a4f53a347b9867770026828125fec
yajamon <yajamon.tatsuki@gmail.com>
2017-07-27 16:47:25 +0900

    Edit install script: install zsh

--- etc/init/yum.sh
+++ etc/init/yum.sh
@@ -12,3 +12,8 @@ sudo yum install -y devtoolset-6
 echo 'source /opt/rh/devtoolset-6/enable' > /tmp/devtoolset-6.sh
 sudo chown root.root /tmp/devtoolset-6.sh
 sudo mv /tmp/devtoolset-6.sh /etc/profile.d/
+
+# Install zsh
+sudo yum install -y zsh
+echo $( cd /etc/yum.repo.d/ && sudo wget http://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/CentOS_${CENTOS_MAJOR}/shells:zsh-users:zsh-completions.repo )
+sudo yum install -y zsh-completions
