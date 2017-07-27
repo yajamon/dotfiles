40ea52369e1e9a4a275a4bedc13009f3cbc4b619
yajamon <yajamon.tatsuki@gmail.com>
2017-07-27 15:17:02 +0900

    Edit install script: execute osx.sh or linux.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ DOT_PATH=$HOME/dotfiles
 
 git clone --recursive $DOTFILES_REPO $DOT_PATH
 $DOT_PATH/setup.sh
+
+source $DOT_PATH/etc/lib/util.sh
+if is_osx; then
+    $DOT_PATH/etc/init/osx.sh
+else
+    $DOT_PATH/etc/init/linux.sh
+fi
