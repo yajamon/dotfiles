f6ca573390d3d9859771b15aa35650c16ddef210
yajamon <yajamon.tatsuki@gmail.com>
2017-09-03 12:27:13 +0900

    install時にbrewを突っ込むようにした

--- etc/init/osx.sh
+++ etc/init/osx.sh
@@ -3,4 +3,7 @@ echo "osx only install script"
 DOT_PATH=$HOME/dotfiles
 INIT_SCRIPTS=$DOT_PATH/etc/init
 
+if ! type -a brew; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 bash $INIT_SCRIPTS/dein.vim.sh
