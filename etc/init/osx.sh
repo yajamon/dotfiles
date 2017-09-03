8e3eb5b2ffbc961cd8bc9bd86c0ed61ecff832cd
yajamon <yajamon.tatsuki@gmail.com>
2017-09-03 12:28:12 +0900

    zshを導入するようにした

--- etc/init/osx.sh
+++ etc/init/osx.sh
@@ -6,4 +6,11 @@ INIT_SCRIPTS=$DOT_PATH/etc/init
 if ! type -a brew; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+if [ ! -e /usr/local/bin/zsh ]; then
+    brew install --without-etcdir zsh
+    brew install zsh-completions
+    chsh -s /usr/local/bin/zsh
+fi
+
 bash $INIT_SCRIPTS/dein.vim.sh
