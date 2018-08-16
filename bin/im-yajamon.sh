88b17cb156d3ef2c282b276f9acc27caf17578b0
yajamon <yajamon.tatsuki@gmail.com>
2018-08-16 13:44:57 +0900

    im-yajamonのname, emailを上書きできるようにした

--- bin/im-yajamon.sh
+++ bin/im-yajamon.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-# Gitリポジトリでyajamonだと設定する
+# getoptsによる引数判定
+while getopts "hn:m:" OPT ; do
+    case $OPT in
+        h) echo "Help: im-yajamon.sh [-n YOUR_NAME -m YOUR_EMAIL]"; exit 1 ;;
+        n) OPT_FLAG_n=1;OPT_VALUE_n=$OPTARG ;;
+        m) OPT_FLAG_m=1;OPT_VALUE_m=$OPTARG ;;
+    esac
+done
+
+if [ ${OPT_FLAG_n:-0} -ne ${OPT_FLAG_m:-0} ]; then
+    echo "Invalid. Look -h option." >&2
+    exit 1
+fi
+
+readonly NAME=${OPT_VALUE_n:-yajamon}
+readonly EMAIL=${OPT_VALUE_m:-yajamon.tatsuki@gmail.com}
+
+# Gitリポジトリで設定する
 if git rev-parse 2> /dev/null; then
-    git config user.name yajamon
-    git config user.email yajamon.tatsuki@gmail.com
+    git config user.name $NAME
+    git config user.email $EMAIL
 fi
