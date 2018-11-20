518a38dc0134ada9521264c36baa21e4bead1345
yajamon <yajamon.tatsuki@gmail.com>
2018-11-20 18:40:19 +0900

    .anyenvが準備できていない環境のことを考慮する

--- etc/setup_scripts/enable_anyenv_completion.sh
+++ etc/setup_scripts/enable_anyenv_completion.sh
@@ -1,10 +1,10 @@
+if [ -e $HOME/.anyenv ]; then
+    if [ -f $HOME/.anyenv/completions/anyenv.zsh ]; then
+        source $HOME/.anyenv/completions/anyenv.zsh
+    fi
 
-if [ -f $HOME/.anyenv/completions/anyenv.zsh ]; then
-    source $HOME/.anyenv/completions/anyenv.zsh
+    for p in $(ls $HOME/.anyenv/envs/*/completions/*.zsh)
+    do
+        source $p
+    done
 fi
-
-for p in $(ls $HOME/.anyenv/envs/*/completions/*.zsh)
-do
-    source $p
-done
-
