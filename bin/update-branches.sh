e94769926e4b1a1f5964f4eb9fc8ae3cf6b7a589
yajamon <yajamon.tatsuki@gmail.com>
2019-10-28 12:09:08 +0900

    複数のbranchを追従させるコマンドを追加
    複数のブランチを並行に走っている状況で複数のマシンを使うとか
    もうわけがわからない状態になるので一気に更新してもらう。

---
+++ bin/update-branches.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+readonly PATTERN=$1
+
+git rev-parse 1>/dev/null || exit 1
+
+readonly Rewind=$(git branch | grep '*' | tr -d '*')
+
+git branch -l $PATTERN | tr '*' ' ' | ( while read bname
+do
+    git switch "$bname" && git merge --ff-only origin/"$bname"
+done
+
+git switch "$Rewind"
+)
