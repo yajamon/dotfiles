8bb00733be2b35b1b6835e14769b16994a68c8b4
yajamon <yajamon.tatsuki@gmail.com>
2022-04-26 04:56:38 +0900

    fix typo

--- etc/init/linux.sh
+++ etc/init/linux.sh
@@ -4,9 +4,9 @@ DOT_PATH=$HOME/dotfiles
 INIT_SCRIPTS=$DOT_PATH/etc/init
 
 if type -a yum >/dev/null 2>&1 ; then
-    echo "Reccomend: let's install tools" >&2
+    echo "Recommended: let's install tools" >&2
     echo "bash $INIT_SCRIPTS/yum.sh" >&2
 fi
 
-echo "Reccomend: dein.vim" >&2
+echo "Recommended: dein.vim" >&2
 echo "bash $INIT_SCRIPTS/dein.vim.sh" >&2
