21da07d857892e459cfa9637c25311848aba6191
yajamon <yajamon.tatsuki@gmail.com>
2022-04-28 02:47:39 +0900

    Edit setup.sh: binの内容に対してシンボリックリンクを生成する

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 readonly DOT_PATH=$HOME/dotfiles
-readonly DOT_FILES=($DOT_PATH/.??* bin)
+readonly DOT_FILES=($DOT_PATH/.??*)
+readonly DOT_BINS=($DOT_PATH/bin/*)
 
 for file in ${DOT_FILES[@]}
 do
@@ -21,4 +22,36 @@ do
     [ -e $HOME/$file ] && echo "exists file: $HOME/$file" 1>&2 && continue
 
     ln -sv $DOT_PATH/$file $HOME/$file
+done
+
+if ! [ -e $HOME/bin ] ; then
+    mkdir -v $HOME/bin
+fi
+if [ -L $HOME/bin ] ; then
+    echo "$HOME/bin is symbolic link" 1>&2
+    echo "abort" 1>&2
+    exit 1
+fi
+if ! [ -d $HOME/bin ] ; then
+    echo "$HOME/bin is not directry" 1>&2
+    echo "abort" 1>&2
+    exit 1
+fi
+
+for bin_item in ${DOT_BINS[@]}
+do
+    bin_item=$(basename $bin_item)
+    src="$DOT_PATH/bin/$bin_item"
+    dest="$HOME/bin/$bin_item"
+
+    if [ -L $dest ] ; then
+        echo "exists link: $dest" 1>&2
+        [ "$(readlink $dest)" != "$src" ] && {
+            echo "but, look different location: $(readlink $dest)"
+        } 1>&2
+        continue
+    fi
+    [ -e $dest ] && echo "exists file: $dest" 1>&2 && continue
+
+    ln -sv $src $dest
 done
