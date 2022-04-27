b912c25bee8347c28fc4a965c7999e7552b4742d
yajamon <yajamon.tatsuki@gmail.com>
2022-04-28 03:09:33 +0900

    Edit setup.sh: .config の中身を展開する
    cargo-atcoder.toml を追加する。

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 readonly DOT_PATH=$HOME/dotfiles
 readonly DOT_FILES=($DOT_PATH/.??*)
 readonly DOT_BINS=($DOT_PATH/bin/*)
+readonly DOT_CONFIG=($DOT_PATH/.config/*)
 
 for file in ${DOT_FILES[@]}
 do
@@ -11,6 +12,7 @@ do
     [[ $file == .git ]] && continue
     [[ $file == .gitignore ]] && continue
     [[ $file == .gitmodules ]] && continue
+    [[ $file == .config ]] && continue
 
     src="$DOT_PATH/$file"
     dest="$HOME/$file"
@@ -45,6 +47,33 @@ do
     bin_item=$(basename $bin_item)
     src="$DOT_PATH/bin/$bin_item"
     dest="$HOME/bin/$bin_item"
+
+    if [ -L $dest ] ; then
+        echo "exists link: $dest" 1>&2
+        [ "$(readlink $dest)" != "$src" ] && {
+            echo "but, look different location: $(readlink $dest)"
+        } 1>&2
+        continue
+    fi
+    [ -e $dest ] && echo "exists file: $dest" 1>&2 && continue
+
+    ln -sv $src $dest
+done
+
+
+if ! [ -e $HOME/.config ] ; then
+    mkdir -v $HOME/.config
+fi
+if ! [ -d $HOME/.config ] ; then
+    echo "$HOME/.config is not directry" 1>&2
+    echo "abort" 1>&2
+    exit 1
+fi
+for config in ${DOT_CONFIG[@]}
+do
+    config=$(basename $config)
+    src="$DOT_PATH/.config/$config"
+    dest="$HOME/.config/$config"
 
     if [ -L $dest ] ; then
         echo "exists link: $dest" 1>&2
